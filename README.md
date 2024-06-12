23d7dc6f18708712d6c95bf1d85598aefe045da6
Rakshit Agarwal <rakshit.agarwal@harness.io>
2024-06-12 11:12:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Gradle-plugin
+
+## Plugin Image
+
+The plugin `anshikaanand/maven-plugin` is available for the following architectures:
+
+| OS            | Tag                                |
+| ------------- | ---------------------------------- |
+| linux/amd64   | `linux-amd64`                      |
+| linux/arm64   | `linux-arm64`                      |
+| windows/amd64 | `windows-amd64`                    |
+
+## Examples
+
+```
+# Plugin YAML
+- step:
+    type: Plugin
+    name: maven-plugin-arm64
+    identifier: maven-plugin-arm64
+    spec:
+        connectorRef: harness-docker-connector
+        image: anshikaanand/maven-plugin:linux-arm64
+       
+
+- step:
+    type: Plugin
+    name: maven-plugin-amd64
+    identifier: maven-plugin-amd64
+    spec:
+        connectorRef: harness-docker-connector
+        image: anshikaanand/maven-plugin:linux-amd64
+        
---
+++ plugin/plugin.go
@@ -0,0 +1,38 @@
+// Copyright 2020 the Drone Authors. All rights reserved.
+// Use of this source code is governed by the Blue Oak Model License
+// that can be found in the LICENSE file.
+
+package plugin
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+// Args provides plugin execution arguments.
+type Args struct {
+	Pipeline
+
+	// Level defines the plugin log level.
+	Level string `envconfig:"PLUGIN_LOG_LEVEL"`
+
+	// TODO replace or remove
+	Param1 string `envconfig:"PLUGIN_PARAM1"`
+	Param2 string `envconfig:"PLUGIN_PARAM2"`
+}
+
+// Exec executes the plugin.
+func Exec(ctx context.Context, args Args) error {
+	// Execute Gradle build command
+	cmd := exec.Command("gradle", "build")
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Println("Error executing Gradle build command:", err)
+		return fmt.Errorf("error executing gradle build command")
+	}
+
+	fmt.Println(string(output))
+	return nil
+}
